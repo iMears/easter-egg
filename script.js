53f9614b2635318875fe6f6e7a677a4565d89db0
imears <maxwellmears@gmail.com>
2016-10-09 19:34:55 -0400

    Change code on every page load

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-cheet('left left right right', function() {
-  $('#canvas').show('fast');
+
+var codeLength = 5
+var arrows     = ['↑', '↓', '←', '→'];
+var randomCode = [];
+
+for (var i = 0; i < codeLength; i += 1) {
+  var randomArrow = arrows[Math.floor(Math.random() * arrows.length)];
+  randomCode.push(randomArrow);
+}
+
+randomCode = randomCode.join(' ');
+
+$(document).ready(function() {
+  $('p').text(randomCode);
+
+  cheet(randomCode, function() {
+    $('p').hide();
+    $('#canvas').show('fast');
+  });
 });
